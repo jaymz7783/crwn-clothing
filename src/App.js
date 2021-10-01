d7fb7ea890e61ea2611dff956418aba569e65040
Tony <tmitchell@greenmountaintechnology.com>
2021-10-01 16:41:05 -0500

    Section 6.83

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import HomePage from './pages/homepage/homepage.component'
-
-import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+
+import './App.css'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
